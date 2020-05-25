0764c94053f5fd80dd49a2c6a1322357c5ffb39e
Seregga09 <65910217+Seregga09@users.noreply.github.com>
2020-05-25 18:08:55 +0300

    Add files via upload

---
+++ Zadanie 4/ConsoleApp3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Внимание!");
+
+            Console.WriteLine($"id потока-{Thread.CurrentThread.ManagedThreadId}");
+
+            List<int> array = new List<int>();
+
+            for (int i = 1; i < 200; i++)
+            {
+                array.Add(i);
+            }
+
+            // параллельная обработка
+            array.AsParallel().ForAll(x =>
+            {
+                double value = Math.Pow(x, 2);
+                Console.WriteLine(
+                    $"Число - {x}, \tквадрат числа - {value}, \tтекущий поток - {Thread.CurrentThread.ManagedThreadId}");
+            });
+
+            Console.WriteLine($"Спасибо за внимание");
+            Console.ReadLine();
+        }
+    }
+}
